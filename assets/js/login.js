ddc8f724d7fd119fed6ac5bfee1e90fa391f1e27
yanxu <531825479@qq.com>
2021-01-03 16:42:48 +0800

    完成了登录和注册功能

---
+++ assets/js/login.js
@@ -0,0 +1,107 @@
+$(function () {
+    // 点击“去注册账号”的链接
+    $('#link_reg').on('click', function () {
+        $('.login-box').hide()
+        $('.reg-box').show()
+    })
+
+    // 点击“去登录”的链接
+    $('#link_login').on('click', function () {
+        $('.login-box').show()
+        $('.reg-box').hide()
+    })
+    var layer = layui.layer
+    var form = layui.form
+    form.verify({
+        // 自定义了一个叫做 pwd 校验规则
+        pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
+        // 校验两次密码是否一致的规则
+        repwd: function (value) {
+            // 通过形参拿到的是确认密码框中的内容
+            // 还需要拿到密码框中的内容
+            // 然后进行一次等于的判断
+            // 如果判断失败,则return一个提示消息即可
+            var pwd = $('.reg-box [name=password]').val()
+            if (pwd !== value) {
+                return '两次密码不一致！'
+            }
+        }
+    })
+
+    form.verify({
+        username: function (value, item) { //value：表单的值、item：表单的DOM对象
+            if (!new RegExp("^[a-zA-Z0-9_\u4e00-\u9fa5\\s·]+$").test(value)) {
+                return '用户名不能有特殊字符';
+            }
+            if (/(^\_)|(\__)|(\_+$)/.test(value)) {
+                return '用户名首尾不能出现下划线\'_\'';
+            }
+            if (/^\d+\d+\d$/.test(value)) {
+                return '用户名不能全为数字';
+            }
+        }
+    });
+    //监听注册表单的提交事件
+    $('#form_reg').on('submit', function (e) {
+        //阻止表单的默认提交行为
+        e.preventDefault()
+        //发起Ajax的POST请求
+        $.ajax({
+            type: 'POST',
+            url: '/api/reguser',
+            data: {
+                username: $('#form_reg [name=username]').val(),
+                password: $('#form_reg [name=password]').val()
+            },
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg(res.message)
+                } else {
+                    layer.msg('注册成功，请登录！')
+                }
+                //让程序模拟人的点击事件
+                $('#link_login').click();
+            }
+        })
+    });
+
+    // //监听登录表单的提交事件
+    // $('#form_login').on('submit', function (e) {
+    //     //阻止表单的默认提交行为
+    //     e.preventDefault()
+    //     //发起Ajax请求
+    //     $.ajax({
+    //         ulr: 'http://ajax.frontend.itheima.net/api/login',
+    //         method: 'POST',
+    //         //快速获取表单数据
+    //         data: $(this).serialize(),
+    //         success: function (res) {
+    //             console.log(res);
+    //         }
+    //     })
+    // })
+
+    // 监听登录表单的提交事件
+    $('#form_login').submit(function (e) {
+        // 阻止默认提交行为
+        e.preventDefault()
+        $.ajax({
+            url: '/api/login',
+            method: 'POST',
+            // 快速获取表单中的数据
+            data: $(this).serialize(),
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('登录失败！')
+                }
+                layer.msg('登录成功！')
+                // 将登录成功得到的 token 字符串，保存到 localStorage 中
+                localStorage.setItem('token', res.token)
+                // 跳转到后台主页
+                location.href = '/index.html'
+            }
+        })
+    })
+
+
+})
